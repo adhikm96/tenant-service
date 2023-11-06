2f274ae4de7bb3b03f03c287f38a3d568b6ceab2
Amit <amit.k@digitalairtech.com>
2023-11-06 12:46:20 +0530

    project created

---
+++ src/main/java/com/thebizio/biziotenantbaseservice/BizioTenantBaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thebizio.biziotenantbaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizioTenantBaseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BizioTenantBaseServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thebizio/biziotenantbaseservice/BizioTenantBaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thebizio.biziotenantbaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizioTenantBaseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
